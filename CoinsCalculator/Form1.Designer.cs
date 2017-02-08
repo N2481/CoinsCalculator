03bae758bc92c0f118872d1b42e5e31ec0d62034
JM <jose.morais@gruporumos.com>
2017-02-08 18:59:55 +0000

    teste

---
+++ CoinsCalculator/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CoinsCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.btCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(53, 16);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(100, 20);
+            this.tbWeight.TabIndex = 0;
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(159, 14);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btCalculate.TabIndex = 1;
+            this.btCalculate.Text = "Calculate";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Weight";
+            // 
+            // lbResult
+            // 
+            this.lbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(15, 53);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(35, 13);
+            this.lbResult.TabIndex = 3;
+            this.lbResult.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 322);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCalculate);
+            this.Controls.Add(this.tbWeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.Button btCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbResult;
+    }
+}
+
---
+++ CoinsCalculator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinsCalculator
+{
+    public partial class Form1 : Form
+    {
+        List<double> coins = new List<double> { 8.5, 7.5, 7.8, 5.74, 4.1, 3.92, 3.06, 2.3 };
+        List<double> values = new List<double> { 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalculate_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            int interation = 0, totalCoins = 0;
+            double weight = 0, w = 0;
+            bool valueFound = false;
+            Double.TryParse(tbWeight.Text, out weight);
+            lbResult.Text = "Calculating...";
+
+            for (int i0 = 0; i0 <= weight / coins[0]; i0++)
+            {
+                for (int i1 = 0; i1 <= weight / coins[1]; i1++)
+                {
+                    for (int i2 = 0; i2 <= weight / coins[2]; i2++)
+                    {
+                        for (int i3 = 0; i3 <= weight / coins[3]; i3++)
+                        {
+                            for (int i4 = 0; i4 <= weight / coins[4]; i4++)
+                            {
+                                for (int i5 = 0; i5 <= weight / coins[5]; i5++)
+                                {
+                                    for (int i6 = 0; i6 <= weight / coins[6]; i6++)
+                                    {
+                                        for (int i7 = 0; i7 <= weight / coins[7]; i7++)
+                                        {
+                                            interation++;
+                                            w = i0 * coins[0] + i1 * coins[1] + i2 * coins[2] + i3 * coins[3] + i4 * coins[4] + i5 * coins[5] + i6 * coins[6] + i7 * coins[7];
+                                            if (w > weight) break;
+
+                                            if (w == weight)
+                                            {
+                                                valueFound = true;
+                                                totalCoins = i0 + i1 + i2 + i3 + i4 + i5 + i6 + i7;
+                                                double totalMoney = i0 * values[0] + i1 * values[1] + i2 * values[2] + i3 * values[3] + i4 * values[4] + i5 * values[5] + i6 * values[6] + i7 * values[7];
+                                                lbResult.Text += string.Format("{0} x 2€ + {1} x 1€ + {2} x 50c€ + {3} x 20c€ + {4} x 10c€ + {5} x 5c€ + {6} x 2c€ + {7} x 1c€ = {8}€ | interations={9} | coins={10}\n\r", i0, i1, i2, i3, i4, i5, i6, i7, totalMoney, interation, totalCoins);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!valueFound) lbResult.Text = "Value not possible";
+        }
+    }
+}
